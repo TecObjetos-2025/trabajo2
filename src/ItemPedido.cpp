8334327b5e998347b01b6fbc5b2b332179eaea4d
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-16 23:24:19 -0500

    Implementacion de ItemPedido. Se actualiza la implementacion para usar punteros y manejar referencias para evitar copias innecesarias

---
+++ src/ItemPedido.cpp
@@ -0,0 +1,18 @@
+#include "ItemPedido.h"
+
+ItemPedido::ItemPedido(const Producto &producto, int cantidad)
+    : producto(producto), cantidad(cantidad) {}
+
+double ItemPedido::getSubtotal() const
+{
+    return producto.getPrecio() * cantidad;
+}
+
+// Getters
+int ItemPedido::getCantidad() const
+{
+    return cantidad;
+}
+
+// Devolver la referencia del Producto
+const Producto &ItemPedido::getProducto() const { return producto; }
